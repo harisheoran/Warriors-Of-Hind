f5eb9186d728b1c12bee62756de3d077dab13771
harisheoran <harisheoran01@gmail.com>
2023-08-10 15:50:01 +0530

    Implement Network Debugger (okhttp + Chucker) & Leak Canary for memork leak check

--- app/src/main/java/com/example/warriorsofhind/WarriorsOfHindApplication.kt
+++ app/src/main/java/com/example/warriorsofhind/WarriorsOfHindApplication.kt
@@ -1,9 +1,17 @@
 package com.example.warriorsofhind
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WarriorsOfHindApplication: Application(){
+class WarriorsOfHindApplication : Application() {
+    companion object {
+        lateinit var myContext: Context
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+        myContext = this
+    }
 }
